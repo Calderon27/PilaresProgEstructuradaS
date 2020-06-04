689a9724cc663aedff1b3d96a8fa4f07e195fe93
carloscalderon <carloscalderon@hotmail.com>
2020-06-04 00:28:27 -0500

    LISTAS DE DATOS

--- PilaresProgEstructurada/frmDatosPersonales.cs
+++ PilaresProgEstructurada/frmDatosPersonales.cs
@@ -20,6 +20,24 @@ namespace PilaresProgEstructurada
         private void txtNombre_KeyDown(object sender, KeyEventArgs e)
         {
 
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                if (this.txtNombre.Text.Length > 0)
+                {
+                    this.lstNombres.Items.Add(this.txtNombre.Text);
+                    this.txtNombre.Text = "";
+                }
+
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            this.lstNombres.Items.Add(this.txtNombre.Text);
+            // 1. validar que haya datos ingresados en la caja de texto
+            // 2. agregar el nombre ingresado en el listbox (clic botón agregar)
+            // 3. el nombre se debe agregar cuando el usuario pulse enter 
+            //    en la caja de texto
         }
     }
 }
